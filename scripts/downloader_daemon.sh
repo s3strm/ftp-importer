71aff21462eafe886b7f73ea94dae4dbd44cf992
s3strm <s3strm@fastmail.com>
2017-05-20 14:33:31 +1000

    Separate the concern of daemon and downloader

--- scripts/downloader_daemon.sh
+++ scripts/downloader_daemon.sh
@@ -1,41 +1,12 @@
 #!/usr/bin/env bash
 QUEUE_URL=TODO
-
-function log() {
-  message=$@
-}
-
-function pop_message() {
-  aws sqs receive-message     \
-    --queue-url ${QUEUE_URL}  \
-    --queue 'Messages[].Body' \
-    --output text
-}
-
-function download() {
-  file=$1
-  log "Downloading ${file} from FTP"
-}
-
-function upload() {
-  file=$1
-  log "Uploading ${file} to S3"
-}
-
-function delete() {
-  file=$1
-  log "Deleting ${file} from FTP"
-}
+DOWNLOADER_BIN="$(dirname $0)/downloader"
 
 while true; do
   file=$(pop_message)
 
-  if [[ -z ${file} ]]; then
-    log "There were no messages on the queue for downloading"
-  else
-    download ${file}
-    upload ${file}
-    delete ${file}
+  if [[ ! -z ${file} ]]; then
+    ${DOWNLOADER_BIN} ${file}
   fi
 
   unset file
