d0411664ebe076e00ca8411d6411626ca63dd8c1
s3strm <s3strm@fastmail.com>
2017-05-20 13:30:25 +1000

    Add some more things to the Lambda

---
+++ lambdas/download_finder/src/lambda.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+import ast
+import boto3
+import os
+import re
+from ftplib import FTP
+
+def downloadable_files():
+    ftp = FTP(os.environ["FTP_HOSTNAME"])
+    ftp.login(
+            os.environ["FTP_USERNAME"],
+            os.environ["FTP_PASSWORD"],
+            )
+
+    ftp.cwd(os.environ["FTP_PATH"])
+    files = ftp.nlst("tt???????.???")
+    ftp.close
+    return files
+
+def add_to_queue(file):
+    client = boto3.client('sqs')
+
+    response = client.send_message(
+        QueueUrl=os.environ["QUEUE_URL"],
+        MessageBody=file,
+    )
+
+
+def lambda_handler(event, context):
+    for file in downloadable_files():
+        print("queuing {} for import".format(file))
+        add_to_queue(file)
+
+if __name__ == "__main__":
+    print(lambda_handler({}, {}))
